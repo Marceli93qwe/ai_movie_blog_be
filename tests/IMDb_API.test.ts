ea1b7a6375a49e40a02f0791bfccea5c0714d90a
Marcel <zseg1ipg.mendrok.marcel@gmail.com>
2023-03-04 23:02:38 +0100

    updated imDb tests

--- tests/IMDb_API.test.ts
+++ tests/IMDb_API.test.ts
@@ -7,8 +7,6 @@ test("IMDB basic connection test", async () => {
     expect(async () => await getMovies()).not.toThrow();
     //returned value should be instaceof Array
     expect(items instanceof Array).toEqual(true);
-    //returned array should have 250 elements
-    expect(items.length).toEqual(250);
 })
 test("Get random movie test", async () => {
     //getRandomMovie() should not throw
