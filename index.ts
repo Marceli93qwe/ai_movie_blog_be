562c642615ad8a864ab9ff7aced5df4d50ab26e7
Marcel <zseg1ipg.mendrok.marcel@gmail.com>
2023-03-04 23:01:52 +0100

    Added basic routing for /review resource

--- index.ts
+++ index.ts
@@ -5,6 +5,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import {handleError} from "./utils/error";
 import {addReviewHandler} from "./lib/addReviewHandler";
+import {ReviewRouter} from "./routers/Review.router";
 
 dotenv.config()
 
@@ -15,19 +16,14 @@ app.use(express.json());
 app.use(cors({
     origin: "http://localhost:3000",
 }));
-
-app.get("/", (req, res) => {
-    throw new Error("xdd");
-    // res.json("works perfectly");
-
-});
+app.use("/review", ReviewRouter);
 
 
 app.use(handleError)
 
 //IT SHOULD add a new review every 24 hours
-// setInterval(addReviewHandler, 86 400 000);
-addReviewHandler()
+setInterval(addReviewHandler, 86400000);
+// addReviewHandler()
 app.listen(3001, "localhost", () => console.log("App listening on http://localhost:3000"));
 
 
