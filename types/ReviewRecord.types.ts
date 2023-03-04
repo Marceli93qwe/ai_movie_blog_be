9797d0366908c0512a4d1f385ced3455d47a0177
Marcel <zseg1ipg.mendrok.marcel@gmail.com>
2023-03-04 11:42:38 +0100

    added Review.record basic features, types and tests

---
+++ types/ReviewRecord.types.ts
@@ -0,0 +1,12 @@
+export interface Review_Record {
+    id: string;
+    rank: number;
+    title: string;
+    fullTitle: string;
+    year: number;
+    image: string;
+    crew: string;
+    imDbRating: number;
+    imDbRatingCount: number;
+    review: string;
+}
