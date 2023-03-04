c6b355c6654b31bc433a5b9a00b630fc9de3fba5
Marcel <zseg1ipg.mendrok.marcel@gmail.com>
2023-03-04 23:03:17 +0100

    added error handler for NotFoundError

--- utils/error.ts
+++ utils/error.ts
@@ -1,8 +1,16 @@
 import {NextFunction, Request, Response} from "express";
 
+export class NotFoundError extends Error {
+}
+
 export const handleError = (err: Error, req: Request, res: Response, next: NextFunction) => {
     console.log(err.message);
     //@TODO Do more advanced error handling
+    if (err instanceof NotFoundError) {
+        res
+            .status(404)
+            .json(err.message)
+    }
     res.status(500);
     res.json("error occurred")
 }
